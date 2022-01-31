9d86b9dcfefec52dcae3ca49159269c06aa77e72
Veys Aliyev <40066034+MDReal32@users.noreply.github.com>
2022-01-31 22:02:46 +0400

    get HTML from entry-server

---
+++ setups/vue3-ts-vite-ssr/server/utils/getHtml.ts
@@ -0,0 +1,48 @@
+import { ServerRenderFunction } from "../../src/types/ServerRenderFunction";
+import { format, Options as PrettierOptions } from "prettier";
+import { minify, Options as MinifierOption } from "html-minifier";
+import { readFileSync } from "fs";
+import { resolve } from "path";
+import { renderPreloadLinks } from "./renderPreloadLinks";
+
+interface Options {
+  render: ServerRenderFunction;
+  isProd: boolean;
+  url: string;
+  template: string;
+}
+
+const prettierOptions: PrettierOptions = {
+  parser: "html",
+  ...JSON.parse(readFileSync(resolve(process.cwd(), ".prettierrc"), "utf-8"))
+};
+const minifierOptions: MinifierOption = {
+  removeComments: true,
+  collapseWhitespace: true,
+  collapseInlineTagWhitespace: true,
+  collapseBooleanAttributes: true,
+  html5: true,
+  sortAttributes: true,
+  keepClosingSlash: true,
+  minifyCSS: true,
+  minifyJS: true,
+  minifyURLs: true,
+  sortClassName: true
+};
+
+export const getHtml = async (options: Options) => {
+  const { render, isProd, url, template } = options;
+  const manifest = isProd ? require("../../build/ssr-manifest.json") : {};
+
+  const { html: renderedHtml, context } = await render(url);
+  const preloadLinks = renderPreloadLinks(context.modules, manifest);
+  const html = template.replace("<!-- app-html -->", renderedHtml).replace(`<!-- preload-links -->`, preloadLinks);
+
+  const minifiedHtml = minify(html, minifierOptions);
+  let finalHtml = minifiedHtml;
+  if (!isProd) {
+    finalHtml = format(minifiedHtml, prettierOptions);
+  }
+
+  return finalHtml;
+};
