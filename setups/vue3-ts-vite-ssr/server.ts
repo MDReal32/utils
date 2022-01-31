f058081e3989734f3f5cfe65a57b70df02f1149f
Veys Aliyev <40066034+MDReal32@users.noreply.github.com>
2022-01-31 22:05:19 +0400

    Get HTML extracted to other function

--- setups/vue3-ts-vite-ssr/server.ts
+++ setups/vue3-ts-vite-ssr/server.ts
@@ -1,13 +1,11 @@
 import express from "express";
 import { resolve as res } from "path";
 import { readFileSync } from "fs";
-import { createServer as viteCreateServer, ViteDevServer, InlineConfig as ViteCreateServerConfig } from "vite";
+import { createServer as viteCreateServer, InlineConfig as ViteCreateServerConfig, ViteDevServer } from "vite";
 import compression from "compression";
 import serveStatic from "serve-static";
-import { Render } from "./src/entry-server";
-import { renderPreloadLinks } from "./server/utils/renderPreloadLinks";
-import { minify, Options as MinifierOption } from "html-minifier";
-import { format, Options as PrettierOptions } from "prettier";
+import { ServerRenderFunction } from "./src/types/ServerRenderFunction";
+import { getHtml } from "./server/utils/getHtml";
 
 const isTest = process.env.NODE_ENV === "test" || !!process.env.VITE_TEST_BUILD;
 const _isProd = process.env.NODE_ENV === "production";
@@ -15,29 +13,9 @@ const _isProd = process.env.NODE_ENV === "production";
 const app = express();
 let vite: ViteDevServer;
 
-const prettierOptions: PrettierOptions = {
-  parser: "html",
-  ...JSON.parse(readFileSync(res(process.cwd(), ".prettierrc"), "utf-8"))
-};
-const minifierOptions: MinifierOption = {
-  removeComments: true,
-  collapseWhitespace: true,
-  collapseBooleanAttributes: true,
-  html5: true,
-  sortAttributes: true
-};
-
 const viteConfig: ViteCreateServerConfig = {
   logLevel: isTest ? "error" : "info",
-  server: {
-    middlewareMode: "ssr",
-    watch: {
-      // During tests we edit the files too fast and sometimes chokidar
-      // misses change events, so enforce polling for consistency
-      usePolling: true,
-      interval: 100
-    }
-  }
+  server: { middlewareMode: "ssr", watch: { usePolling: true, interval: 100 } }
 };
 
 const resolve = (...p: string[]) => res(__dirname, ...p);
@@ -46,7 +24,6 @@ export const createServer = async (root = process.cwd(), isProd = _isProd) => {
   viteConfig.root = root;
 
   // @ts-ignore
-  const manifest = isProd ? require("./dist/client/ssr-manifest.json") : {};
   const indexProd = isProd ? readFileSync(resolve("dist/client/index.html"), "utf-8") : "";
 
   if (isProd) {
@@ -59,7 +36,7 @@ export const createServer = async (root = process.cwd(), isProd = _isProd) => {
 
   app.use("*", async (req, res) => {
     const { originalUrl: url } = req;
-    let template: string, render: Render;
+    let template: string, render: ServerRenderFunction;
 
     try {
       if (isProd) {
@@ -71,17 +48,9 @@ export const createServer = async (root = process.cwd(), isProd = _isProd) => {
         render = (await vite.ssrLoadModule("/src/entry-server.ts")).render;
       }
 
-      const { html: renderedHtml, context } = await render(url);
-      const preloadLinks = renderPreloadLinks(context.modules, manifest);
-      const html = template.replace("<!-- app-html -->", renderedHtml).replace(`<!-- preload-links -->`, preloadLinks);
+      const html = await getHtml({ url, render, isProd, template });
 
-      const minifiedHtml = minify(html, minifierOptions);
-      let finalHtml = minifiedHtml;
-      if (!isProd) {
-        finalHtml = format(minifiedHtml, prettierOptions);
-      }
-
-      res.status(200).set({ "Content-Type": "text/html" }).end(finalHtml);
+      res.status(200).set({ "Content-Type": "text/html" }).end(html);
     } catch (e) {
       if (e instanceof Error) {
         vite && vite.ssrFixStacktrace(e);
