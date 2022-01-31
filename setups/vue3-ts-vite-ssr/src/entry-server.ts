9d9980886dca0d4be25794059513725212751043
Veys Aliyev <40066034+MDReal32@users.noreply.github.com>
2022-01-31 22:06:41 +0400

    Type extracted to another Function

--- setups/vue3-ts-vite-ssr/src/entry-server.ts
+++ setups/vue3-ts-vite-ssr/src/entry-server.ts
@@ -1,17 +1,12 @@
 import { renderToString, SSRContext } from "vue/server-renderer";
 import { createApp } from "./main";
 import { router } from "./router";
+import { ServerRenderFunction } from "./types/ServerRenderFunction";
 
-export interface RenderFunction {
-  html: string;
-  context: SSRContext;
-}
-export type Render = (url: string) => Promise<RenderFunction>;
-
-export const render: Render = async (url: string) => {
+export const render: ServerRenderFunction = async (url: string) => {
   await router.push(url);
   await router.isReady();
-  const app = createApp();
+  const app = await createApp();
 
   const context: SSRContext = {};
   const html = await renderToString(app, context);
