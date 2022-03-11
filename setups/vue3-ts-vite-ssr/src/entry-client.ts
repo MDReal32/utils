276a7975918cc2d073f1a198d0d2c39b59d347e7
MDReal <veysaliyev00@gmail.com>
2022-03-11 17:24:47 +0400

    Data as global Variable

--- setups/vue3-ts-vite-ssr/src/entry-client.ts
+++ setups/vue3-ts-vite-ssr/src/entry-client.ts
@@ -4,5 +4,6 @@ import { router } from "./router";
 (async () => {
   const app = createApp();
   await router.isReady();
+  app.config.globalProperties = JSON.parse(document.getElementById("__DATA__")?.textContent!);
   app.mount("#app");
 })();
